327323171e3cea633515604accaf2d861086678b
Gideon Tong <gideon@gideontong.com>
2020-08-15 01:22:38 -0700

    Revert "Update counts"
    This reverts commit bea72ebb9748fa0ac401445d0f53ba32e1c3826d.

--- lib/scripts/countVerses.py
+++ lib/scripts/countVerses.py
@@ -1,14 +1,13 @@
 import json
 
 bible = json.loads(open('NIV.json').read())['bible']
-names = json.loads(open('bible.json').read())['bookmap']
 meta = {
 }
 # 66
-for book in names.keys():
+for book in bible:
     meta[book] = {}
-    for chapter in bible[names[book]['NIV']]:
-        meta[book][chapter] = len(bible[names[book]['NIV']][chapter])
+    for chapter in bible[book]:
+        meta[book][chapter] = len(bible[book][chapter])
 
-with open('counts_updated.json', 'w') as file:
+with open('counts.json', 'w') as file:
     json.dump(meta, file)
